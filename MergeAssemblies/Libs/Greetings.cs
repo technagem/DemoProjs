7a97e38e3bb2884c595d0188390e1bd8754405e1
Technagem <dadossan@microsoft.com>
2021-11-28 17:25:24 -0500

    demo projects created

---
+++ MergeAssemblies/Libs/Greetings.cs
@@ -0,0 +1,11 @@
+﻿using Technagem.Tools;
+
+namespace Technagem.Libs
+{
+    public static class Greetings
+    {
+        public static string SayHiTo(string str) => str.SayHi();
+
+        public static string SayHelloTo(string str) => str.SayHello();
+    }
+}
---
+++ MergeAssemblies/Tools/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Technagem.Tools
+{
+    public static class StringExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static string SayHi(this String str)
+        {
+            return $"Hi {str}!";
+        }
+
+        public static string SayHello(this String str)
+        {
+            return $"Hello {str}!";
+        }
+    }
+}
